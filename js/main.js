329edf0189d7ae7f59173d8586dc793e2c327ebb
glyman01 <glyman@frontlinetechnologies.com>
2016-03-22 13:31:05 -0400

    Fine tuning load screen animations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-//dom ready
-$(document).on('ready', function{
+// DOM
+$(document).on('ready', function(){
+	
+	var navHeight = $('.nav-wrapper').outerHeight(true);
+	$('body').css('margin-top', navHeight);
 
-}); //end dom
+	var navClass = $('.nav-main a');
+
+	// Hard coded
+	// $('.js-scroll').on('click', function(){
+	// 	$.scrollTo('#Random', 800);
+	// })
+
+	// Global scrollTo click event
+	navClass.on('click', function(e){
+		e.preventDefault();
+		// console.log($(this).attr('href'))
+
+		navClass.removeClass('is-current');
+
+		$(this).addClass('is-current');
+
+		var whereToScroll = $(this).attr('href');
+
+		$.scrollTo(whereToScroll, {
+			duration: 500,
+			offset: -navHeight
+		})
+	})
+
+
+
+
+
+}); // end doc on ready
