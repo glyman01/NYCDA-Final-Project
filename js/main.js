a8fc5a8ec15049a0448730da6742e112b83fe0f3
glyman01 <glyman@frontlinetechnologies.com>
2016-03-31 04:59:21 -0400

    Added Swiper slider, custom slider nav icons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 // DOM
 $(document).on('ready', function(){
 	
+	// initializing swiper
+	
+	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        // slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+	});
+
+    var swiperV = new Swiper('.swiper-container-v', {
+        pagination: '.swiper-pagination-v',
+        paginationClickable: true,
+        direction: 'vertical',
+        spaceBetween: 30,
+        // slidesPerView: 1,
+        loop: true
+    });
+
+
 	var navHeight = $('.nav-wrapper').outerHeight(true);
 
 	var navClass = $('.nav-main a.js-internal, .page-nav-svg, .js-section-btn');
