d6e50e8cb523000d01e6028b81aeeceeacd0ae2a
glyman01 <glyman@frontlinetechnologies.com>
2016-03-31 20:11:20 -0400

    Fixed Swiper Nav, almost ready for prod

--- js/main.js
+++ js/main.js
@@ -38,6 +38,8 @@ $(document).on('ready', function(){
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
         loop: true
     });
 
