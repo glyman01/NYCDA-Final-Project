d7cdd729e1bbae90a2e50f33a77bd77cb2d410be
Geoff Lyman <gtdesigns27@gmail.com>
2018-02-12 20:41:22 -0500

    Stat bars
    Updated CSS and JS related to about percentages

--- js/main.js
+++ js/main.js
@@ -67,6 +67,12 @@ $(document).on('ready', function(){
 
 		$(this).addClass('is-current');
 
+
+		if ($(this) === '#About') {
+
+
+		}
+
 		
 
 		$.scrollTo(whereToScroll, {
@@ -117,126 +123,177 @@ $(document).on('ready', function(){
 
 
 	// Animating on scroll position
-	$('#stats').addClass('hidden-animation').viewportChecker({
-        classToAdd: 'animation-begin visible-animation',
-        offset: 100
-    });
+	// $('#stats').addClass('hidden-animation').viewportChecker({
+ //        classToAdd: 'animation-begin visible-animation',
+ //        offset: 100
+ //    });
 
-    $('#page-nav').addClass('start-svg-animate').viewportChecker({
-        classToAdd: 'stop-svg-animate',
-        classToRemove: 'start-svg-animate',
-        offset: 400
-    });
+ //    $('#page-nav').addClass('start-svg-animate').viewportChecker({
+ //        classToAdd: 'stop-svg-animate',
+ //        classToRemove: 'start-svg-animate',
+ //        offset: 400
+ //    });
 
-    // $('.stat-bars').addClass('hidden-main-content').viewportChecker({
-    //     classToAdd: 'is-playing',
-    //     classToRemove: 'hidden-main-content',
-    //     offset: 400
-    // });
+ //    // $('.stat-bars').addClass('hidden-main-content').viewportChecker({
+ //    //     classToAdd: 'is-playing',
+ //    //     classToRemove: 'hidden-main-content',
+ //    //     offset: 400
+ //    // });
 
-	// Delaying the stat bar animation - this works, but needs improvement		
-	function animateStats(){
+	// // Delaying the stat bar animation - this works, but needs improvement		
+	// function animateStats(){
 
-		$('.stat-bars').addClass('hidden-main-content').viewportChecker({
-	        classToAdd: 'is-playing',
-	        classToRemove: 'hidden-main-content',
-	        offset: 400
-	    });
-
-		// if class .stat-bars has class .hidden-main-content, do this
-		if (!$('.stat-bars').hasClass('hidden-main-content')){
-
-			// if screen comes into view do this
-			var statAnimate = setTimeout(function(){
-
-				// this sets the class to js-animate on scroll
-				$('.stat-bars').viewportChecker({
-					classToAdd: 'js-animate',
-					offset: 400
-				});
-				
-
-				$('.stat-bars').delay(1500).queue(function(next){
-					$(this).addClass('is-playing');
-					next();
-				});
-
-				// This runs if screen is in view
-
-				if ($('#animate-stats').hasClass('js-animate')){
-						
-					$('.stat-bars').delay(2000).queue(function(next){
-						$(this).removeClass('hidden-main-content');
-						next();
-					});
-
-					$('.stat-bars').delay(1500).queue(function(next){
-						$(this).addClass('is-playing');
-						next();
-					});
-				}
-
-			});
-
-		} 
-
-		// else {
-
-		// 		var statEngage = setTimeout(function(){
-
-		// 			// this sets the class to js-animate on scroll
-		// 			$('.stat-bars').viewportChecker({
-		// 				classToAdd: 'is-playing',
-		// 				offset: 400
-		// 			});
-
-		// 			// This runs if screen is in view
-
-		// 			// if (!$('.stat-bars').hasClass('is-playing')){
-							
-		// 			// 	$('.stat-bars').delay(100).queue(function(next){
-		// 			// 		$(this).addClass('is-playing');
-		// 			// 		next();
-		// 			// 	});
-		// 			// }
-
-		// 		});
-		// 	}
-	};
-
-	// Delaying the stat bar animation - this works, but needs improvement		
-	// function statsEngage(){
+	// 	$('.stat-bars').addClass('hidden-main-content').viewportChecker({
+	//         classToAdd: 'is-playing',
+	//         classToRemove: 'hidden-main-content',
+	//         offset: 400
+	//     });
 
 	// 	// if class .stat-bars has class .hidden-main-content, do this
-	// 	if ($('.stat-bars').hasClass('engage')){
-						
+	// 	if (!$('.stat-bars').hasClass('hidden-main-content')){
+
 	// 		// if screen comes into view do this
-	// 		var statEngage = setTimeout(function(){
+	// 		var statAnimate = setTimeout(function(){
 
 	// 			// this sets the class to js-animate on scroll
 	// 			$('.stat-bars').viewportChecker({
-	// 				classToRemove: 'hidden-main-content',
+	// 				classToAdd: 'js-animate',
 	// 				offset: 400
+	// 			});
+				
+
+	// 			$('.stat-bars').delay(1500).queue(function(next){
+	// 				$(this).addClass('is-playing');
+	// 				next();
 	// 			});
 
 	// 			// This runs if screen is in view
 
-	// 			if (!$('.stat-bars').hasClass('is-playing')){
+	// 			if ($('#animate-stats').hasClass('js-animate')){
 						
-	// 				$('.stat-bars').delay(100).queue(function(next){
+	// 				$('.stat-bars').delay(2000).queue(function(next){
+	// 					$(this).removeClass('hidden-main-content');
+	// 					next();
+	// 				});
+
+	// 				$('.stat-bars').delay(1500).queue(function(next){
 	// 					$(this).addClass('is-playing');
 	// 					next();
 	// 				});
 	// 			}
 
 	// 		});
-	// 	}
 
+	// 	} 
+
+	// 	// else {
+
+	// 	// 		var statEngage = setTimeout(function(){
+
+	// 	// 			// this sets the class to js-animate on scroll
+	// 	// 			$('.stat-bars').viewportChecker({
+	// 	// 				classToAdd: 'is-playing',
+	// 	// 				offset: 400
+	// 	// 			});
+
+	// 	// 			// This runs if screen is in view
+
+	// 	// 			// if (!$('.stat-bars').hasClass('is-playing')){
+							
+	// 	// 			// 	$('.stat-bars').delay(100).queue(function(next){
+	// 	// 			// 		$(this).addClass('is-playing');
+	// 	// 			// 		next();
+	// 	// 			// 	});
+	// 	// 			// }
+
+	// 	// 		});
+	// 	// 	}
 	// };
 
+	// // Delaying the stat bar animation - this works, but needs improvement		
+	// // function statsEngage(){
 
-	// statsEngage();
-	animateStats();
+	// // 	// if class .stat-bars has class .hidden-main-content, do this
+	// // 	if ($('.stat-bars').hasClass('engage')){
+						
+	// // 		// if screen comes into view do this
+	// // 		var statEngage = setTimeout(function(){
+
+	// // 			// this sets the class to js-animate on scroll
+	// // 			$('.stat-bars').viewportChecker({
+	// // 				classToRemove: 'hidden-main-content',
+	// // 				offset: 400
+	// // 			});
+
+	// // 			// This runs if screen is in view
+
+	// // 			if (!$('.stat-bars').hasClass('is-playing')){
+						
+	// // 				$('.stat-bars').delay(100).queue(function(next){
+	// // 					$(this).addClass('is-playing');
+	// // 					next();
+	// // 				});
+	// // 			}
+
+	// // 		});
+	// // 	}
+
+	// // };
+
+
+	// // statsEngage();
+	// animateStats();
+
+	$('#stats').addClass('hidden-animation');
+	$('.stat-bars').addClass('hidden-main-content');
+
+
+	// Nav ScrollTo
+
+	var navHeight = $('.nav-wrapper').outerHeight(true);
+
+	var navClass = $('.nav-main a.js-internal, .page-nav-svg, .js-section-btn');
+
+	// Global scrollTo click event
+
+	navClass.on('click', function(e){
+		var whereToScroll = $(this).attr('href');
+
+		e.preventDefault();
+
+		navClass.removeClass('is-current');
+
+		$(this).addClass('is-current');		
+
+		$.scrollTo(whereToScroll, {
+			duration: 600,
+		})
+
+		if ($(this) === '#About') {
+			$('#stats').removeClass('hidden-animation').addClass('animation-begin visible-animation');
+			$('.stat-bars').removeClass('hidden-main-content').addClass('is-playing');
+		};
+	})
+
+
+	$(window).scroll(function() {    
+
+	    var navScroll = $(window).scrollTop();
+
+	    if (navScroll >= 100) {
+	        $(".js-scroll").addClass('planet-nav');
+	        $('.js-nav-reset').css({
+	   			height: '0'
+	   		});
+	    } else {
+	    	$('.js-scroll').removeClass('planet-nav');
+	    	$('.js-nav-reset').css({
+	   			position: 'absolute',
+	   			height: 'auto',
+	   		});
+	    }
+
+	});
 
     
 
