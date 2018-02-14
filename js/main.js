d6710feb89ad171d86b495fb92aaef589a917119
Geoff Lyman <gtdesigns27@gmail.com>
2018-02-14 07:59:43 -0500

    Updated Viewport Checker
    Refactored scroll load for about section

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $(document).on('ready', function(){
 		        	});
 		    	}
 		    
-		    }, 3400);
+		    }, 2800);
 
 
 		// otherwise, if the cookie exists do this -
@@ -50,54 +50,41 @@ $(document).on('ready', function(){
         loop: true
     });
 
-    // Nav ScrollTo
-
-	var navHeight = $('.nav-wrapper').outerHeight(true);
-
-	var navClass = $('.nav-main a.js-internal, .page-nav-svg, .js-section-btn');
-
-	// Global scrollTo click event
-
-	navClass.on('click', function(e){
-		var whereToScroll = $(this).attr('href');
-
-		e.preventDefault();
-
-		navClass.removeClass('is-current');
-
-		$(this).addClass('is-current');
-
-
-		if ($(this) === '#About') {
-
-
+	// Select all links with hashes
+	$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function(event) {
+		// On-page links
+		if (
+		  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+		  && 
+		  location.hostname == this.hostname
+		) {
+		  // Figure out element to scroll to
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		  // Does a scroll target exist?
+		  if (target.length) {
+		    // Only prevent default if animation is actually gonna happen
+		    event.preventDefault();
+		    $('html, body').animate({
+		      scrollTop: target.offset().top
+		    }, 600, function() {
+		      // Callback after animation
+		      // Must change focus!
+		      var $target = $(target);
+		      $target.focus();
+		      if ($target.is(":focus")) { // Checking if the target was focused
+		        return false;
+		      } else {
+		        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+		        $target.focus(); // Set focus again
+		      };
+		    });
+		  }
 		}
-
-		
-
-		$.scrollTo(whereToScroll, {
-			duration: 600,
-		})
-	})
-
-
-	$(window).scroll(function() {    
-
-	    var navScroll = $(window).scrollTop();
-
-	    if (navScroll >= 100) {
-	        $(".js-scroll").addClass('planet-nav');
-	        $('.js-nav-reset').css({
-	   			height: '0'
-	   		});
-	    } else {
-	    	$('.js-scroll').removeClass('planet-nav');
-	    	$('.js-nav-reset').css({
-	   			position: 'absolute',
-	   			height: 'auto',
-	   		});
-	    }
-
 	});
 
 	// NASA Apod API
@@ -119,133 +106,138 @@ $(document).on('ready', function(){
 
 		    $('.block-nasa').css('background', 'url(' + result.url + ') top center no-repeat');
 		}	
-	}
+	};
+
+	$('#page-nav').viewportChecker();
+	$('.stat-bars').viewportChecker();
+	$('#stats').viewportChecker();
 
 
 	// Animating on scroll position
+	// $('#page-nav').addClass('start-svg-animate').viewportChecker({
+	//        classToAdd: 'stop-svg-animate',
+	//        classToRemove: 'start-svg-animate',
+	//        offset: 400
+	//    });	
+
 	// $('#stats').addClass('hidden-animation').viewportChecker({
- //        classToAdd: 'animation-begin visible-animation',
- //        offset: 100
- //    });
+	//        classToAdd: 'animation-begin visible-animation',
+	//        offset: 100
+	//    });
 
- //    $('#page-nav').addClass('start-svg-animate').viewportChecker({
- //        classToAdd: 'stop-svg-animate',
- //        classToRemove: 'start-svg-animate',
- //        offset: 400
- //    });
 
- //    // $('.stat-bars').addClass('hidden-main-content').viewportChecker({
- //    //     classToAdd: 'is-playing',
- //    //     classToRemove: 'hidden-main-content',
- //    //     offset: 400
- //    // });
+	   // $('.stat-bars').addClass('hidden-main-content').viewportChecker({
+	   //     classToAdd: 'is-playing',
+	   //     classToRemove: 'hidden-main-content',
+	   //     offset: 400
+	   // });
 
-	// // Delaying the stat bar animation - this works, but needs improvement		
-	// function animateStats(){
+	// Delaying the stat bar animation - this works, but needs improvement		
+	function animateStats(){
 
-	// 	$('.stat-bars').addClass('hidden-main-content').viewportChecker({
-	//         classToAdd: 'is-playing',
-	//         classToRemove: 'hidden-main-content',
-	//         offset: 400
-	//     });
+		// $('.stat-bars').addClass('hidden-main-content').viewportChecker({
+	 //        classToAdd: 'is-playing',
+	 //        classToRemove: 'hidden-main-content',
+	 //        offset: 400
+	 //    });
+
+		// if class .stat-bars has class .hidden-main-content, do this
+		if (!$('.stat-bars').hasClass('hidden-main-content')){
+
+			// if screen comes into view do this
+			var statAnimate = setTimeout(function(){
+
+				// this sets the class to js-animate on scroll
+				// $('.stat-bars').viewportChecker({
+				// 	classToAdd: 'js-animate',
+				// 	offset: 400
+				// });
+				
+
+				$('.stat-bars').delay(1500).queue(function(next){
+					$(this).addClass('is-playing');
+					next();
+				});
+
+				// This runs if screen is in view
+
+				if ($('#animate-stats').hasClass('js-animate')){
+						
+					$('.stat-bars').delay(2000).queue(function(next){
+						$(this).removeClass('hidden-main-content');
+						next();
+					});
+
+					$('.stat-bars').delay(1500).queue(function(next){
+						$(this).addClass('is-playing');
+						next();
+					});
+				}
+
+			});
+
+		} 
+
+		// else {
+
+		// 		var statEngage = setTimeout(function(){
+
+		// 			// this sets the class to js-animate on scroll
+		// 			$('.stat-bars').viewportChecker({
+		// 				classToAdd: 'is-playing',
+		// 				offset: 400
+		// 			});
+
+		// 			// This runs if screen is in view
+
+		// 			// if (!$('.stat-bars').hasClass('is-playing')){
+							
+		// 			// 	$('.stat-bars').delay(100).queue(function(next){
+		// 			// 		$(this).addClass('is-playing');
+		// 			// 		next();
+		// 			// 	});
+		// 			// }
+
+		// 		});
+		// 	}
+	};
+
+	// Delaying the stat bar animation - this works, but needs improvement		
+	// function statsEngage(){
 
 	// 	// if class .stat-bars has class .hidden-main-content, do this
-	// 	if (!$('.stat-bars').hasClass('hidden-main-content')){
-
+	// 	if ($('.stat-bars').hasClass('engage')){
+						
 	// 		// if screen comes into view do this
-	// 		var statAnimate = setTimeout(function(){
+	// 		var statEngage = setTimeout(function(){
 
 	// 			// this sets the class to js-animate on scroll
 	// 			$('.stat-bars').viewportChecker({
-	// 				classToAdd: 'js-animate',
+	// 				classToRemove: 'hidden-main-content',
 	// 				offset: 400
-	// 			});
-				
-
-	// 			$('.stat-bars').delay(1500).queue(function(next){
-	// 				$(this).addClass('is-playing');
-	// 				next();
 	// 			});
 
 	// 			// This runs if screen is in view
 
-	// 			if ($('#animate-stats').hasClass('js-animate')){
+	// 			if (!$('.stat-bars').hasClass('is-playing')){
 						
-	// 				$('.stat-bars').delay(2000).queue(function(next){
-	// 					$(this).removeClass('hidden-main-content');
-	// 					next();
-	// 				});
-
-	// 				$('.stat-bars').delay(1500).queue(function(next){
+	// 				$('.stat-bars').delay(100).queue(function(next){
 	// 					$(this).addClass('is-playing');
 	// 					next();
 	// 				});
 	// 			}
 
 	// 		});
+	// 	}
 
-	// 	} 
-
-	// 	// else {
-
-	// 	// 		var statEngage = setTimeout(function(){
-
-	// 	// 			// this sets the class to js-animate on scroll
-	// 	// 			$('.stat-bars').viewportChecker({
-	// 	// 				classToAdd: 'is-playing',
-	// 	// 				offset: 400
-	// 	// 			});
-
-	// 	// 			// This runs if screen is in view
-
-	// 	// 			// if (!$('.stat-bars').hasClass('is-playing')){
-							
-	// 	// 			// 	$('.stat-bars').delay(100).queue(function(next){
-	// 	// 			// 		$(this).addClass('is-playing');
-	// 	// 			// 		next();
-	// 	// 			// 	});
-	// 	// 			// }
-
-	// 	// 		});
-	// 	// 	}
 	// };
 
-	// // Delaying the stat bar animation - this works, but needs improvement		
-	// // function statsEngage(){
 
-	// // 	// if class .stat-bars has class .hidden-main-content, do this
-	// // 	if ($('.stat-bars').hasClass('engage')){
-						
-	// // 		// if screen comes into view do this
-	// // 		var statEngage = setTimeout(function(){
+	// statsEngage();
+	animateStats();
 
-	// // 			// this sets the class to js-animate on scroll
-	// // 			$('.stat-bars').viewportChecker({
-	// // 				classToRemove: 'hidden-main-content',
-	// // 				offset: 400
-	// // 			});
-
-	// // 			// This runs if screen is in view
-
-	// // 			if (!$('.stat-bars').hasClass('is-playing')){
-						
-	// // 				$('.stat-bars').delay(100).queue(function(next){
-	// // 					$(this).addClass('is-playing');
-	// // 					next();
-	// // 				});
-	// // 			}
-
-	// // 		});
-	// // 	}
-
-	// // };
-
-
-	// // statsEngage();
-	// animateStats();
-
-	$('#stats').addClass('hidden-animation');
-	$('.stat-bars').addClass('hidden-main-content');
+	// $('#stats').addClass('hidden-animation');
+	// $('.stat-bars').addClass('hidden-main-content');
 
 
 	// Nav ScrollTo
@@ -265,9 +257,9 @@ $(document).on('ready', function(){
 
 		$(this).addClass('is-current');		
 
-		$.scrollTo(whereToScroll, {
-			duration: 600,
-		})
+		// $.scrollTo(whereToScroll, {
+		// 	duration: 600,
+		// })
 
 		if ($(this) === '#About') {
 			$('#stats').removeClass('hidden-animation').addClass('animation-begin visible-animation');
